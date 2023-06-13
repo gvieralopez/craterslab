37d180084e28a5af0002451d8cf86c52097213ae
Gustavo Viera López <gvieralopez@gmail.com>
2023-06-13 12:35:35 +0200

    Rework project structure

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
-from lidar import load_from_file
-from craters import Crater
+from pycraters.lidar import load_from_file
+from pycraters.craters import Crater
 
 # Define data sources
 after_impact_data = load_from_file('craterexp1.mat')
@@ -13,8 +13,7 @@ image_depth = 1.0  # Amount of mm on every increment on image color
 c = Crater(before_impact_data, after_impact_data, image_resolution, image_depth)
 
 # Plot the crater in 3D
-preview_scale = (1, 1, 10)
-c.plot_3D(title='Crater view in 3D', preview_scale=preview_scale)
+c.plot_3D(title='Crater view in 3D', preview_scale=(1, 1, 10))
 
 # Plot a transversal cut of the crater
 c.set_profile(start_point=(0, 0), end_point=(100, 100))
