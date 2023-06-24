7623bb63b1321baf6111317a039782eabce0ef65
Gustavo Viera López <gvieralopez@gmail.com>
2023-06-24 11:41:02 +0200

    Improve ellipse fitting with better initial guess

--- pycraters/elipse.py
+++ pycraters/elipse.py
@@ -15,8 +15,12 @@ def fit_elipse(x: np.ndarray, y: np.ndarray) -> tuple:
         ellipse = ((x_hat / a)**2 + (y_hat / b)**2) - 1
         return np.sum(ellipse**2)
 
-    # Define the initial guess for the ellipse parameters
-    params0 = [1, 1, 0, 0, 0]
+    # Compute the mean and standard deviation of the data
+    x_mean, y_mean = np.mean(x), np.mean(y)
+    x_std, y_std = np.std(x), np.std(y)
+
+    # Define the initial guess for the ellipse parameters based on the mean and standard deviation of the data
+    params0 = [x_std, y_std, x_mean, y_mean, 0]
 
     # Fit the ellipse to the points using the objective function and the initial guess
     result = minimize(objective, params0, args=(x, y))
@@ -25,7 +29,7 @@ def fit_elipse(x: np.ndarray, y: np.ndarray) -> tuple:
     return result.x
 
 if __name__ == "__main__":
-    points = [(93.58558558558559, 50.0), (8.806806806806819, 50.000000000000014), (88.49249249249249, 76.11711711711712), (15.067067067067057, 22.08708708708707), (66.37337337337337, 91.7917917917918), (39.114114114114116, 6.60660660660659), (39.850850850850854, 91.09109109109109), (65.63663663663664, 10.5105105105105), (17.825825825825827, 75.88288288288288), (84.24824824824827, 27.006006006005997)]
+    points = [(118.65465465465465, 76.0), (33.82582582582583, 76.00000000000001), (108.87687687687688, 107.18318318318319), (37.525525525525495, 55.2912912912913), (80.43943943943944, 119.8958958958959), (53.2122122122122, 37.12512512512512), (53.76276276276276, 113.2012012012012), (80.08908908908909, 33.16916916916918), (39.37537537537538, 95.36336336336336), (105.30930930930931, 47.41141141141142)]
     x = np.array([p[0] for p in points])
     y = np.array([p[1] for p in points])
     a, b, cx, cy, theta = fit_elipse(x, y)
