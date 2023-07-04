6028195f6ddcfc49a266acbd11cfd836999f0267
Frank.CMachin <frankcm.work@gmail.com>
2023-07-03 22:39:59 -0600

    files .mat fix it

--- example.py
+++ example.py
@@ -2,8 +2,8 @@ from pycraters.lidar import load_from_file
 from pycraters.craters import Crater
 
 # Define data sources
-after_impact_data = load_from_file('craterexp37.mat')
-before_impact_data = load_from_file('planoexp37.mat')
+after_impact_data = load_from_file('crater_aFig4.mat')
+before_impact_data = load_from_file('plano_aFig4.mat')
 
 # Define physical constants
 image_resolution = 2.8025  # Distance in mm between adjacent pixels 
@@ -13,7 +13,7 @@ image_depth = 1.0  # Amount of mm on every increment on image color
 c = Crater(before_impact_data, after_impact_data, image_resolution, image_depth)
 
 # Plot the crater in 3D
-#c.plot_3D(title='Crater view in 3D', preview_scale=(1, 1, 4))
+c.plot_3D(title='Crater view in 3D', preview_scale=(1, 1, 4))
 
 # Plot a transversal cut of the crater
 #c.plot_profile('Crater profile')
